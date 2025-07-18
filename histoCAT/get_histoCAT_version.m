3392f2d6596ece315f3260b022c87c9e87f69cf8
Yusuf Turhan <yusufozgurtr@gmail.com>
2025-07-18 10:45:57 +0200

    revert git versioning as matlab doesnt support that format

--- histoCAT/get_histoCAT_version.m
+++ histoCAT/get_histoCAT_version.m
@@ -1,15 +1,3 @@
 function histoCAT_version = get_histoCAT_version()
-    base_version = '1.78';
-    
-    % Get the first seven characters of the current commit hash
-    [status, cmdout] = system('git rev-parse --short HEAD');
-    
-    if status == 0 % Command executed successfully
-        commit_hash_short = strtrim(cmdout); % Remove any leading/trailing whitespace
-        histoCAT_version = [base_version, '.', commit_hash_short];
-    else
-        % Handle case where Git is not found or command fails
-        warning('Git command failed or not found. Using base version only.');
-        histoCAT_version = base_version;
-    end
+    histoCAT_version = '1.78';
 end
